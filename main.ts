ac1eb64b021efeaf2e7f266151074b6b66b3a156
jannis0901 <jannis.fromm09@icloud.com>
2026-02-24 15:20:11 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (Modus == 1) {
         basic.showNumber(radio.receivedPacket(receivedNumber))
+        basic.pause(3000)
+        basic.clearScreen()
+        radio.sendNumber(receivedNumber)
     }
+})
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    radio.sendNumber(0 - 1)
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (Modus == 0) {
@@ -15,10 +21,16 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         radio.sendNumber(3)
     }
 })
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    radio.sendNumber(0 + 1)
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    radio.sendNumber(2)
+})
 let Modus = 0
 Modus = 0
 radio.setGroup(97)
-radio.setTransmitPower(6)
+radio.setTransmitPower(7)
 basic.forever(function () {
     if (Modus == 0) {
         basic.setLedColor(0xff0000)
